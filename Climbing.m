f0f8a127fd2942fd34306dca7e68a6f77a94e755
JD <jherlehy@uwo.ca>
2022-03-31 21:03:33 -0400

    climber

--- Climbing.m
+++ Climbing.m
@@ -2,14 +2,22 @@
 %set the vars
 %Cim for the CIM Motor climbing for just the situation of climbing
 InputClimbing = 1;
-loadInertiaClimbing = 0.2;
-mechDampingConstantClimbing = 0.01; 
+loadInertiaClimbing = 0.05;
+mechDampingConstantClimbing = 0.1; 
 TerminalResistanceCim = 3;
 TerminalInductanceCim = 122/1000000;
 TorqueCoefCim = 18.803/1000;
 TorqueDisturbanceCim = 0;
 EMFCoefCim = 18.803/1000;
+ResistorTF = 5/(2*pi*1); %E/(2*pi*N) E: reference voltage, N: # of avalible turns
 load_system('ClimbSim.slx');
 sim('ClimbSim.slx');
-Simulink.sdi.view;
-climbingPos = Simulink.sdi.snapshot("from", "opened", "to", "figure")
+%Simulink.sdi.view;
+%snap = Simulink.sdi.CustomSnapshot;
+%snap.Rows = 1;
+%snap.YRange = {[0, 100]};
+%climbingPos = Simulink.sdi.snapshot("from", "custom", "to", "figure", "settings", snap)
+plot(ans.ScopeData.time, ans.ScopeData.signals(2).values)
+hold on
+plot(ans.ScopeData.time, ans.ScopeData.signals(1).values)
+hold off
