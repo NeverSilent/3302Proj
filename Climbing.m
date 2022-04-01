75dd36dd176d315c171e4bfb9b1b505d1e690080
JD <jherlehy@uwo.ca>
2022-04-01 01:09:07 -0400

    sdrg

--- Climbing.m
+++ Climbing.m
@@ -3,12 +3,12 @@
 %Cim for the CIM Motor climbing for just the situation of climbing
 InputClimbing = 1;
 loadInertiaClimbing = 6.12; %%see document ref
-mechDampingConstantClimbing = 2; 
 TerminalResistanceCim = 3;
 TerminalInductanceCim = 122/1000000;
 TorqueCoefCim = 18.803/1000;
 TorqueDisturbanceCim = 0;
 EMFCoefCim = 18.803/1000;
+mechDampingConstantClimbing = (TorqueCoefCim / TerminalResistanceCim) / EMFCoefCim; %%see document ref
 ResistorTF = 5/(2*pi*1); %E/(2*pi*N) E: reference voltage, N: # of avalible turns
 load_system('ClimbSim.slx');
 sim('ClimbSim.slx');
