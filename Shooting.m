1e6ce5a0aec21e7ffb7e0e63310b9920c6bb0208
ericsuszek <80979066+ericsuszek@users.noreply.github.com>
2022-04-02 22:49:51 -0400

    Update Shooting.m

--- Shooting.m
+++ Shooting.m
@@ -5,20 +5,16 @@ ActuatedMass = 0.0084; %kg, From solidworks
 pressure_in = 413685; %pascal
 
 A = Bore^2/4; %bore area
-V0 = A*0.1524;
-m = 0.0084;
-kc = 8.14;
-
-kq = 0.62;
-b = 0.5;
-
-k2 = 1/b;
-
-beta = 142000;
-
-
-tm = m/b;
-th = V0/(2*beta*kc);
+V0 = A*0.1524; %Initial Volume
+m = 0.0084; %mass 
+kc = 8.14; %constant
+kq = 0.62; %constant
+b = 0.147; %damping coefficient
+k1 = 1/kc; %tf constant
+k2 = 1/b; %tf constant
+beta = 142000; %bulk modulus
+tm = m/b; %tf constant
+th = V0/(2*beta*kc); %tf constat
 
 
 
