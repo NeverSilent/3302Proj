8eca72c29c725e9e5609debbfa124a10bf5c4517
ericsuszek <80979066+ericsuszek@users.noreply.github.com>
2022-04-02 22:33:25 -0400

    Update Shooting.m

--- Shooting.m
+++ Shooting.m
@@ -4,3 +4,22 @@ ActuatedMass = 0.0084; %kg, From solidworks
 
 pressure_in = 413685; %pascal
 
+A = Bore^2/4; %bore area
+V0 = A*0.1524;
+m = 0.0084;
+kc = 8.14;
+
+kq = 0.62;
+b = 0.5;
+
+k2 = 1/b;
+
+beta = 142000;
+
+
+tm = m/b;
+th = V0/(2*beta*kc);
+
+
+
+
